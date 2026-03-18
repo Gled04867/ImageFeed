755c5271d58f64edaeb075b12991ba8610cb6ef2
Gled04867 <Gled04867@ya.ru>
2026-03-18 23:56:35 +0300

    Выполнено задание Спринт №9

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    private lazy var imageView = UIImageView()
+    private lazy var nameLabel = UILabel()
+    private lazy var loginLabel = UILabel()
+    private lazy var statusLabel = UILabel()
+    private lazy var button = UIButton(type: .system)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupImage()
+        setupNameLabel()
+        setupLoginLabel()
+        setupStatusLabel()
+        setupButton()
+        
+    }
+    
+    
+    private func setupImage() {
+        imageView.image = UIImage(resource: .avatar)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+    }
+    
+    private func setupNameLabel() {
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = .ypWhite
+        nameLabel.font = .systemFont(ofSize: 23, weight: .bold)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+    }
+    
+    private func setupLoginLabel() {
+        loginLabel.text = "@ekaterina_nov"
+        loginLabel.textColor = .ypGray
+        loginLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginLabel)
+        
+        loginLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        loginLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor).isActive = true
+    }
+    
+    private func setupStatusLabel() {
+        statusLabel.text = "Hello, world!"
+        statusLabel.textColor = .ypWhite
+        statusLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(statusLabel)
+        
+        statusLabel.leadingAnchor.constraint(equalTo: loginLabel.leadingAnchor).isActive = true
+        statusLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8).isActive = true
+        statusLabel.trailingAnchor.constraint(equalTo: loginLabel.trailingAnchor).isActive = true
+    }
+    
+    private func setupButton() {
+        button.setImage(UIImage(systemName: "ipad.and.arrow.forward"), for: .normal)
+        button.tintColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+    }
+}
